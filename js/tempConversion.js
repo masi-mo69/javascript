0bc2e2d75e286438f2e81dedcd2fbcd0594f7ed2
massomeh <ms.mohammadi69@gmail.com>
2024-08-28 16:26:11 +0330

    first commit2

--- js/tempConversion.js
+++ js/tempConversion.js
@@ -2,46 +2,34 @@ let $ = document
 
 const firstValue = $.querySelector('.C')
 const secondValue = $.querySelector('.F')
-const converter = $.querySelector('#converter')
+const converter = $.getElementById('#converter')
 const result = $.querySelector('.result')
 const convertButton = $.querySelector('.convertButton')
 const resetButton = $.querySelector('.resetButton')
 const changeButton = $.querySelector('.changeButton')
 
 
-function convert(){
-    // console.log('convert');
-    if(converter.value === ''){
-        result.innerHTML = 'Insert Correct Value'
-        result.style.color = '#993300'
-    } else {
-        if(firstValue.innerHTML === '°C'){
-            let resultValue = (converter.value * 1.8) + 32
-            result.innerHTML = converter.value + '°C To ' + resultValue + '°F'
 
-        }
-    }
+function convert(){
+
 }
+
 function reset(){
-    // console.log('reset');
-    result.innerHTML = ''
-    converter.value = ''
+
 }
+
 function swap(){
-    // console.log('swap');
+    // console.log('change')
     if(firstValue.innerHTML === '°C'){
         firstValue.innerHTML = '°F'
         secondValue.innerHTML ='°C'
-        converter.setAttribute('placeholder' , '°F')
+        converter.setAttribute('placeholder', '°F')
         document.title = 'SalzLearn| Js | °F to °C'
-        
-
     } else {
         firstValue.innerHTML = '°C'
-        secondValue.innerHTML = '°F'
+        secondValue.innerHTML ='°F'
         converter.setAttribute('placeholder' , '°C')
         document.title = 'SalzLearn| Js | °C to °F'
-
     }
 }
 
